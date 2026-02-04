77cba14e2b389d6f0c34df6da2cc5932f1148308
manideep81215 <gannamanideep18@gmail.com>
2026-02-04 19:14:08 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { useContext } from "react";
-import { ContextProvider } from "./context/Context.jsx";
+import { AppProvider } from "./context/Context.jsx";
 // import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
